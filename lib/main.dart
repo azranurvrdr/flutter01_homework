10f02c6e73c47e8de78289b5ab085cc73349f74c
azranurvardar <149239437+azranurvrdr@users.noreply.github.com>
2023-10-28 14:02:22 +0300

    'ABC' butonu 'ekle' olarak degistirildi

--- lib/main.dart
+++ lib/main.dart
@@ -115,7 +115,7 @@ class _MyHomePageState extends State<MyHomePage> {
             TextButton(
                 onPressed: _incrementCounter,
                 child: Text(
-                  'ABC',
+                  'ekle',
                 )
             ),
           ],
