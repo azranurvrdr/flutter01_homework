e865a049c8271e92a2685174c5b649fee6db2432
azranurvardar <149239437+azranurvrdr@users.noreply.github.com>
2023-10-31 02:39:42 +0300

    'Center' kısmında resim eklemek için yaptığım şeylerden uyarı alıyorum. İnternettende yardım almaya çalıştım ama hala sorunlu ne yapacağımı bilmiyorum şuan.

--- lib/main.dart
+++ lib/main.dart
@@ -55,18 +55,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<String> resimIcerik = [
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  ];
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -87,9 +80,10 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
+
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
+        child: const Column(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
@@ -104,28 +98,18 @@ class _MyHomePageState extends State<MyHomePage> {
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            TextButton(
-                onPressed: _incrementCounter,
-                child: Text(
-                  'eklendi',
+            itemCount: resimIcerik.length,
+            itemBuilder: (context, index){
+              children: [
+                Card(
+                  child:
+                  Image.asset("images/bayrak_2.png"),
                 )
-            ),
-          ],
+              ];
+            }
+
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
