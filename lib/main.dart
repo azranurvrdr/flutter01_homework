5dad036cccc1a01d04f531f31108702a2ff96363
azranurvardar <149239437+azranurvrdr@users.noreply.github.com>
2023-11-08 19:11:17 +0300

    Geri dönme butonunu olmadan olan versiyon.

--- lib/main.dart
+++ lib/main.dart
@@ -53,133 +53,100 @@ class _MyHomePageState extends State<MyHomePage> {
             GestureDetector(
               onTap: () {
                 // Navigate to the new page
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) {
-                        return Scaffold(
-                          appBar: AppBar(
-                            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-                            title: Text(
-                              'Adding Page',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                ),
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) {
+                    return Scaffold(
+                        appBar: AppBar(
+                          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+                          title: Text(
+                            'Adding Page',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                          body: Center(
-                            child: Column(
-                              children: [
-                                TextField(
-                                  controller: _nameController,
-                                  decoration: InputDecoration(
-                                    labelText: 'İsim',
-                                    hintText: 'İsminizi girin',
-                                  ),
+                        ),
+                        body: Center(
+                          child: Column(
+                            children: [
+                              TextField(
+                                controller: _nameController,
+                                decoration: InputDecoration(
+                                  labelText: 'İsim',
+                                  hintText: 'İsminizi girin',
                                 ),
-                                TextField(
-                                  controller: _ageController,
-                                  keyboardType: TextInputType.number,
-                                  decoration: InputDecoration(
-                                    labelText: 'Yaş',
-                                    hintText: 'Yaşınızı girin',
-                                  ),
+                              ),
+                              TextField(
+                                controller: _ageController,
+                                keyboardType: TextInputType.number,
+                                decoration: InputDecoration(
+                                  labelText: 'Yaş',
+                                  hintText: 'Yaşınızı girin',
                                 ),
-                                ElevatedButton(
-                                    onPressed: () {
-                                      //Fieldların boş olup olmadığını kontrol ediyoruz
-                                      if(_nameController.text.isEmpty || _ageController.text.isEmpty) {
-                                        //Hata mesajı bastırıyoruz
-                                        showDialog(
-                                            context: context,
-                                            builder: (context) {
-                                              return AlertDialog(
-                                                title: Text('!HATA!'),
-                                                content: Text(
-                                                  'Lütfen isim ve yaş bilgisini birlikte girin!'
-                                                ),
-                                                actions: [
-                                                  TextButton(
-                                                      onPressed: () {
-                                                        Navigator.pop(context);
-                                                      },
-                                                      child: Text('Tamam'),
-                                                  ),
-                                                ],
-                                              );
-                                            },
-                                        );
-                                      } else {
-                                        //Alınan verileri listeye ekliyoruz
-                                        userData.add({
-                                          'name': _nameController.text,
-                                          'age': int.parse(_ageController.text),
-                                        });
-
-                                        //Verileri kaydettikten sonra TextFieldı temizliyoruz.
-                                        _nameController.clear();
-                                        _ageController.clear();
-
-                                        //Verileri ekrana bastırıyoruz.
-                                        ScaffoldMessenger.of(context)
-                                        .showSnackBar(
-                                          SnackBar(
-                                              content: Text('Kişi başarıyla eklendi'),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  //Fieldların boş olup olmadığını kontrol ediyoruz
+                                  if(_nameController.text.isEmpty || _ageController.text.isEmpty) {
+                                    //Hata mesajı bastırıyoruz
+                                    showDialog(
+                                      context: context,
+                                      builder: (context) {
+                                        return AlertDialog(
+                                          title: Text('!HATA!'),
+                                          content: Text(
+                                              'Lütfen isim ve yaş bilgisini birlikte girin!'
                                           ),
+                                          actions: [
+                                            TextButton(
+                                              onPressed: () {
+                                                Navigator.pop(context);
+                                              },
+                                              child: Text('Tamam'),
+                                            ),
+                                          ],
                                         );
-                                      }
-                                    },
-                                    child: Text('EKLE'),
+                                      },
+                                    );
+                                  } else {
+                                    //Alınan verileri listeye ekliyoruz
+                                    userData.add({
+                                      'name': _nameController.text,
+                                      'age': int.parse(_ageController.text),
+                                    });
+
+                                    //Verileri kaydettikten sonra TextFieldı temizliyoruz.
+                                    _nameController.clear();
+                                    _ageController.clear();
+
+                                    //Verileri ekrana bastırıyoruz.
+                                    ScaffoldMessenger.of(context)
+                                        .showSnackBar(
+                                      SnackBar(
+                                        content: Text('Kişi başarıyla eklendi'),
+                                      ),
+                                    );
+                                  }
+                                },
+                                child: Text('EKLE'),
+                              ),
+                              Expanded(
+                                child: ListView.builder(
+                                  itemCount: userData.length,
+                                  itemBuilder: (context, index) {
+                                    final user = userData[index];
+                                    return ListTile(
+                                      title: Text(user['name']),
+                                      subtitle: Text('Yaş: ${user['age']}'),
+                                    );
+                                  },
                                 ),
-
-
-                                  ElevatedButton(
-                                    onPressed: () {
-                                      if(userData.length >= 3){
-                                        Navigator.pop(context);
-                                      } else {
-                                        showDialog(
-                                            context: context,
-                                            builder: (context) {
-                                              return AlertDialog(
-                                                title: Text('!HATA!'),
-                                                content: Text('Listeye en az 3 kişi eklemelisiniz!'),
-                                                actions: [
-                                                  TextButton(
-                                                      onPressed: () {
-                                                        Navigator.of(context).pop();
-                                                      },
-                                                      child: Text('Tamam'),
-                                                  ),
-                                                ],
-                                              );
-                                            },
-                                        );
-                                      }
-                                    },
-                                    child: Text('Geri Dön'),
-                                ),
-
-                                  ListView.builder(
-                                    itemCount: userData.length,
-                                    itemBuilder: (context, index) {
-                                      final user = userData[index];
-                                      //Her kişiyi Row olarak oluşturuyoruz.
-                                      return Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text(user['name']),
-                                          Text('Age: ${user['age']}'),
-                                        ],
-                                      );
-                                    },
-                                  ),
-
-
-                              ],
-                            ),
-                          )
-                        );
-                    },
-                  )
+                              ),
+                            ],
+                          ),
+                        )
+                    );
+                  },
+                )
                 );
               },
               child: Card(
