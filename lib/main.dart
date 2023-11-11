968ea51424aa23900336f8b63c012a282e9b9d57
azranurvardar <149239437+azranurvrdr@users.noreply.github.com>
2023-11-11 16:37:14 +0300

    Düzenleme version 3

--- lib/main.dart
+++ lib/main.dart
@@ -96,8 +96,8 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              ElevatedButton(
-                onPressed: () {
+              GestureDetector(
+                onTap: () {
                   Navigator.of(context).push(MaterialPageRoute(
                       builder: (context) {
                         return Scaffold(
