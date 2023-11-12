a49bf7c5d3d0e8fdb899f129e00438cd377a3b42
azranurvardar <149239437+azranurvrdr@users.noreply.github.com>
2023-11-12 12:21:12 +0300

    Sayfa geçişleri düzgün çalışıyor sadece geri dönme koşullarını ekleyeceğim.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-
 import 'package:flutter/material.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class Person {
@@ -12,47 +12,36 @@ class Person {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Adding App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red.shade700),
-        useMaterial3: true,
+        primarySwatch: Colors.purple,
       ),
-      home: const MyHomePage(title: 'Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  //Hilda
   final List<Person> people = [];
-  int personNumber = 0;
-
-
   final nameController = TextEditingController();
   final ageController = TextEditingController();
 
-  void _addPerson(){
+  void _addPerson() {
     final name = nameController.text;
     final age = ageController.text;
 
-    if(name.isEmpty || age.isEmpty || !isNumeric(age)){
+    if (name.isEmpty || age.isEmpty || !isNumeric(age)) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text('Geçerli bir isim ve yaş girin!'),
+          content: Text('Geçerli isim ve yaş bilgisi girin!'),
         ),
       );
     } else {
@@ -68,159 +57,169 @@ class _MyHomePageState extends State<MyHomePage> {
     return value != null && int.tryParse(value) != null;
   }
 
-
-  void _NavigateToNewPage(){
-    if(people.length >= 3) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => NewPage(people: people)),
-      );
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('3 kişi ekleyin'),
-        ),
-      );
-    }
+  void _navigateToAddPage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => AddingPage(people: people)),
+    );
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text('TÜRK BAYRAĞI'),
-        ),
-        body: Center (
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) {
-                        return Scaffold(
-                          appBar: AppBar(
-                            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-                            title: Text(
-                              'Adding Page',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          body: Center(
-                            child: Column(
-                              //Hilda
-                              children: [
-                                Row(
-                                  children: [
-                                    Expanded(
-                                      child: TextField(
-                                        controller: nameController,
-                                        decoration: InputDecoration(labelText: 'İsim'),
-                                      ),
-                                    ),
-                                    SizedBox(width: 15),
-                                    Expanded(
-                                      child: TextField(
-                                        controller: ageController,
-                                        decoration: InputDecoration(labelText: 'Yaş'),
-                                        keyboardType: TextInputType.number,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                ElevatedButton(
-                                  onPressed: _addPerson,
-                                  child: Text('Ekle'),
-                                ),
-
-
-                                ElevatedButton(
-                                  onPressed: _NavigateToNewPage,
-                                  child: Text('Geri Dön'),
-                                ),
-
-
-
-
-                                Expanded(
-                                  child: ListView.builder(
-                                    itemCount: people.length,
-                                    itemBuilder: (context, index) {
-                                      final person = people[index];
-                                      return ListTile(
-                                        title: Text('${person.name} ${person.age}'),
-                                      );
-                                    },
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      }
-                  )
-                  );
-                },
-
-
-                child: Card(
-                  child: SizedBox(
-                    child: Image(image:
-                    AssetImage('images/bayrakkk.jpg'),
-                    ),
+      appBar: AppBar(
+        title: Text('TÜRK BAYRAĞI'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            GestureDetector(
+              onTap: _navigateToAddPage,
+              child: Card(
+                child: SizedBox(
+                  child: Image(
+                    image: AssetImage('images/bayrakkk.jpg'),
                   ),
                 ),
               ),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  ListTile(
+                    title: Text('ÖZELLİKLER'),
+                  ),
+                  ListTile(
+                    title: Text('1) Ekranda Türk bayrağı resmi bulunuyor.'),
+                  ),
+                  ListTile(
+                    title: Text('2) Türk bayrağı bir hilal ve bir adet beş köşeli yıldızdan oluşur.'),
+                  ),
+                  ListTile(
+                    title: Text('3) Bayrağın rengi kırmızıdır ve üzerindeki ay-yıldızın rengi beyazdır.'),
+                  ),
+                  ListTile(
+                    title: Text('4) Türk bayrağı, 29 Mayıs 1936 da kabul edilmiştir.'),
+                  ),
+                  ListTile(
+                    title: Text('5) Bayrak, Türkiyenin sınırları içinde ve dışında resmi olarak kullanılır.'),
+                  ),
+                  ListTile(
+                    title: Text('6) Türk bayrağı, 20. yüzyılın en tanınmış bayraklarından biridir.'),
+                  ),
+                  ListTile(
+                    title: Text('7) Bayrak, genellikle kamusal binalarda, okullarda ve resmi törenlerde görülür.'),
+                  ),
+                  ListTile(
+                    title: Text('8) Bayrağın kabul edildiği 29 Mayıs, Türkiye de "Bayram" olarak kutlanır.'),
+                  ),
+                  ListTile(
+                    title: Text('9) Bayrağın tasarımı, İstiklal Savaşı nın sembolizmini yansıtır.'),
+                  ),
+                  ListTile(
+                    title: Text('10) Bayrak, Türkiye nin ulusal egemenliği ve bağımsızlığını sembolize eder.'),
+                  ),
 
-              Expanded(
-                child: ListView(
-                  children: [
-                    ListTile(
-                      title: Text('OZELLİKLER'),
-                    ),
-                    ListTile(
-                      title: Text('1) Ekranda Türk bayrağı resmi bulunuyor.'),
-                    ),
-                    ListTile(
-                      title: Text('2) Türk bayrağı bir hilal ve bir adet beş köşeli yıldızdan oluşur.'),
-                    ),
-                    ListTile(
-                      title: Text('3) Bayrağın rengi kırmızıdır ve üzerindeki ay-yıldızın rengi beyazdır.'),
-                    ),
-                    ListTile(
-                      title: Text('4) Türk bayrağı, 29 Mayıs 1936 da kabul edilmiştir.'),
-                    ),
-                    ListTile(
-                      title: Text('5) Bayrak, Türkiyenin sınırları içinde ve dışında resmi olarak kullanılır.'),
-                    ),
-                    ListTile(
-                      title: Text('6) Türk bayrağı, 20. yüzyılın en tanınmış bayraklarından biridir.'),
-                    ),
-                    ListTile(
-                      title: Text('7) Bayrak, genellikle kamusal binalarda, okullarda ve resmi törenlerde görülür.'),
-                    ),
-                    ListTile(
-                      title: Text('8) Bayrağın kabul edildiği 29 Mayıs, Türkiye de "Bayram" olarak kutlanır.'),
-                    ),
-                    ListTile(
-                      title: Text('9) Bayrağın tasarımı, İstiklal Savaşı nın sembolizmini yansıtır.'),
-                    ),
-                    ListTile(
-                      title: Text('10) Bayrak, Türkiye nin ulusal egemenliği ve bağımsızlığını sembolize eder.'),
-                    ),
-
-                  ],
-                ),
+                ],
               ),
-            ],
-          ),
-        )
+            ),
+          ],
+        ),
+      ),
     );
+  }
+}
 
+class AddingPage extends StatefulWidget {
+  final List<Person> people;
 
+  AddingPage({required this.people});
+
+  @override
+  _AddingPageState createState() => _AddingPageState();
+}
+
+class _AddingPageState extends State<AddingPage> {
+  final nameController = TextEditingController();
+  final ageController = TextEditingController();
+
+  void _addPerson() {
+    final name = nameController.text;
+    final age = ageController.text;
+
+    if (name.isEmpty || age.isEmpty || !isNumeric(age)) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Geçerli isim ve yaş bilgisi girin!'),
+        ),
+      );
+    } else {
+      setState(() {
+        widget.people.add(Person(name: name, age: int.parse(age)));
+        nameController.clear();
+        ageController.clear();
+      });
+    }
+  }
+
+  bool isNumeric(String value) {
+    return value != null && int.tryParse(value) != null;
+  }
+
+  void _navigateBack() {
+    Navigator.pop(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Adding Page'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(labelText: 'İsim'),
+                  ),
+                ),
+                SizedBox(width: 15),
+                Expanded(
+                  child: TextField(
+                    controller: ageController,
+                    decoration: InputDecoration(labelText: 'Yaş'),
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: _addPerson,
+              child: Text('Ekle'),
+            ),
+            ElevatedButton(
+              onPressed: _navigateBack,
+              child: Text('Geri Dön'),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: widget.people.length,
+                itemBuilder: (context, index) {
+                  final person = widget.people[index];
+                  return ListTile(
+                    title: Text('${person.name} ${person.age}'),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
